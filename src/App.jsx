acb27088293310fc903dfd7b4b48da8e51f05c44
DiptiVats <diptivats6@gmail.com>
2023-11-20 10:25:50 +0530

    alsmost done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,49 @@ import { useState } from "react";
 import NewProject from "./component/NewProjects";
 import NoPorjectSelected from "./component/NoPorjectSelected";
 import ProjectsSideBar from "./component/ProjectsSideBar";
+import SelectedProject from "./component/SelectedProject";
 
 function App() {
   const [projectsState, setProjectState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
+
+  function handleAddTask(text) {
+    setProjectState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        ...prevState,
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks],
+      };
+    });
+  }
+
+  function handleDeleteTask(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        projects: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+
+  function handleSelectProject(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
   function handleStartAddProject() {
     setProjectState((prevState) => {
       return {
@@ -28,19 +65,44 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
+      const pojectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: pojectId,
       };
       return {
         ...prevState,
-        projects: [...prevState.projects, newProject],
         selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
       };
     });
   }
 
-  let content;
+  function handleDeleteProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject
+      project={selectedProject}
+      onDelete={handleDeleteProject}
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
   if (projectsState.selectedProjectId === null) {
     content = (
       <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -53,6 +115,8 @@ function App() {
       <ProjectsSideBar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
--- src/component/SelectedProject.jsx
+++ src/component/SelectedProject.jsx
@@ -1,9 +1,17 @@
-export default function SelectedProject({ project }) {
-  const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
+import Tasks from "./Task";
+
+export default function SelectedProject({
+  project,
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks,
+}) {
+  /*const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
     day: "numeric",
-  });
+  });*/
   return (
     <div className="w-[35rem] mt-16">
       <header className="pb-4 mb-4 border-b-2 border-stone-300 ">
@@ -11,12 +19,19 @@ export default function SelectedProject({ project }) {
           <h1 className="text-3xl font-bold text-stone-600 mb-2">
             {project.title}
           </h1>
-          <button>DELETE</button>
+          <button
+            onClick={onDelete}
+            className="text-stone-600 hover:text-stone-950"
+          >
+            DELETE
+          </button>
         </div>
-        <p>{formattedDate}</p>
-        <p>{project.description}</p>
+        <p className="mb-4 text-stone-400">{project.dueDate}</p>
+        <p className="text-stone-600 whitespace-pre-wrap">
+          {project.description}
+        </p>
       </header>
-      TASKS
+      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
     </div>
   );
 }
